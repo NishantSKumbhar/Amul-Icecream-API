ace08e602a1a7fb8f01d03d9efadce33df7c6bc6
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-17 08:14:15 +0530

    Add project files.

---
+++ Amul/Controllers/IcecremeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IcecremeController : ControllerBase
+    {
+    }
+}
